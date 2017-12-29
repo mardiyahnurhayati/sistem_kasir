627729534aae73304b25021b9c7d0546ca09005d
Mardiyah Nurhayati <mardiyah.nurhayati@gmail.com>
2017-12-29 17:09:30 +0700

    first commit

---
+++ application/views/admin/motor_edit.php
@@ -0,0 +1,57 @@
+   
+
+        <div class="col-sm-9 col-sm-offset-3 col-lg-10 col-lg-offset-2 main">
+        <div class="row">
+            <ol class="breadcrumb">
+                <li><a href="#">
+                    <em class="fa fa-home"></em>
+                </a></li>
+                <li class="active">Data Motor</li>
+            </ol>
+        </div><!--/.row-->
+        
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">Edit Data Motor</h1>
+            </div>
+        </div><!--/.row-->    
+
+        <div class="container">
+        <div class="panel panel-default">  
+            <div class="panel panel-body">
+            <form>
+          <div class="form-group row">
+            <label for="plat" class="col-sm-2 col-form-label">Plat Motor</label>
+            <div class="col-sm-10">
+              <input type="text"  class="form-control" id="platmotor" placeholder="">
+            </div>
+          </div>
+
+          <div class="form-group row">
+            <label for="merek" class="col-sm-2 col-form-label">Merek</label>
+            <div class="col-sm-10">
+              <input type="text"  class="form-control" id="id_merk" placeholder="">
+            </div>
+          </div>
+
+          <div class="form-group row">
+            <label for="warna" class="col-sm-2 col-form-label">Warna</label>
+            <div class="col-sm-10">
+              <input type="text"  class="form-control" id="warna" placeholder="">
+            </div>
+          </div>
+
+           <div class="form-group row">
+            <label for="warna" class="col-sm-2 col-form-label">Tahun Terbit</label>
+            <div class="col-sm-10">
+              <input type="text"  class="form-control" id="tahunterbit" placeholder="">
+            </div>
+          </div>
+          <button type="button" class="btn btn-primary pull-right">Save</button>
+        </form>
+        </div>                      
+        </div>
+        </div>
+        </div>
+        
+</div>  <!--/.main-->
